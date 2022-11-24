00ddc97efac80a92f9d29a22f5f77fa7eaa04f1b
Puputfatmawati <puputfatma31@gmail.com>
2022-11-24 09:18:46 +0700

    add database

---
+++ controllers/adminController.js
@@ -0,0 +1,14 @@
+module.exports = {
+    getAllAdmin: async (req, res) => {
+      
+    },
+    getAdminByID: async (req, res) => {
+      
+    },
+    deleteAdminByID: async (req, res) => {
+      
+    },
+    updateAdminByID: async (req, res) => {
+      
+    },
+  };
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const makananRouter = require('./makananRouter')
+const userRouter = require('./userRouter')
+const adminRouter = require('./adminRouter')
+const auths = require('./authRouter');
 
 // localhots:3000
 router.get("/", (req, res) => {
@@ -9,6 +12,14 @@ router.get("/", (req, res) => {
 });
 // "/makanan"
 router.get('/makanan', makananRouter)
+
+// User
+router.get('/user', userRouter)
+
+// admin
+router.get('/admin', adminRouter)
+
+router.use('/auth-users', auths)
 
 // Eksport router 
 module.exports = router
