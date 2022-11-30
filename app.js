c880e2cc0abfea4703cf6bb582a8cfccb0f69225
Nunung Ali Maulana <nunungalimaulana@students.unnes.ac.id>
2022-11-30 22:30:55 +0700

    koneksi ke db 2

--- app.js
+++ app.js
@@ -3,25 +3,24 @@ const app = express();
 const DBconnection = require("./config/config.json");
 const allRoutes = require("./routes");
 const PORT = process.env.PORT || 3000;
-const Sequelize = require('Sequelize');
-const sequelize = new Sequelize('calcare_dev', 'root', '12345678', {
-  host: '127.0.0.1',
-  dialect:'mysql'
+const Sequelize = require("Sequelize");
+const sequelize = new Sequelize("calcare_dev", "root", "12345678", {
+  host: "127.0.0.1",
+  dialect: "mysql",
 });
 
 // Connect to DB
 try {
   sequelize.authenticate(DBconnection);
-  console.log('Connection has been established successfully.');
+  console.log("Connection has been established successfully.");
+  // Middleware
+  app.use(express.json());
+  app.use(allRoutes);
+
+  // Menampilkan Koneksi Server
+  app.listen(PORT, () => {
+    console.log(`Calcare Server is running at port http://localhost:${PORT}`);
+  });
 } catch (error) {
-  console.error('Unable to connect to the database:', error);
+  console.error("Unable to connect to the database:", error);
 }
-
-// Middleware
-app.use(express.json());
-app.use(allRoutes);
-
-// Menampilkan Koneksi Server
-app.listen(PORT, () => {
-  console.log(`Calcare Server is running at port http://localhost:${PORT}`);
-});
