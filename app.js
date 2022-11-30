7cd1dfc16bb071a90a2ad63f5de2979731e93159
Nunung Ali Maulana <nunungalimaulana@students.unnes.ac.id>
2022-11-30 22:15:26 +0700

    Koneksi ke db

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
-// const DBconnection = require("./config/config");
+const DBconnection = require("./config/config.json");
 const allRoutes = require("./routes");
 const PORT = process.env.PORT || 3000;
+const Sequelize = require('Sequelize');
+const sequelize = new Sequelize('calcare_dev', 'root', '12345678', {
+  host: '127.0.0.1',
+  dialect:'mysql'
+});
+
+// Connect to DB
+try {
+  sequelize.authenticate(DBconnection);
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 // Middleware
 app.use(express.json());
