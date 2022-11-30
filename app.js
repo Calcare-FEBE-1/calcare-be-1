f95440b01ee39300d25ea5cf654957d188183f75
Nunung Ali Maulana <nunungalimaulana@students.unnes.ac.id>
2022-11-30 22:37:07 +0700

    perbaiki port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const DBconnection = require("./config/config.json");
 const allRoutes = require("./routes");
-const PORT = process.env.PORT || 3000;
+const { } = process.env || 3000;
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize("calcare_dev", "root", "12345678", {
   host: "127.0.0.1",
@@ -13,14 +13,15 @@ const sequelize = new Sequelize("calcare_dev", "root", "12345678", {
 try {
   sequelize.authenticate(DBconnection);
   console.log("Connection has been established successfully.");
-  // Middleware
-  app.use(express.json());
-  app.use(allRoutes);
-
-  // Menampilkan Koneksi Server
-  app.listen(PORT, () => {
-    console.log(`Calcare Server is running at port http://localhost:${PORT}`);
-  });
 } catch (error) {
   console.error("Unable to connect to the database:", error);
 }
+
+// Middleware
+app.use(express.json());
+app.use(allRoutes);
+
+// Menampilkan Koneksi Server
+app.listen(PORT, () => {
+  console.log(`Calcare Server is running at port http://localhost:${PORT}`);
+});
